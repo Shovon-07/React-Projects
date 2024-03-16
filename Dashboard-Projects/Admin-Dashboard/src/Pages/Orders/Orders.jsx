67d89784cc2a18eb6e4f8c1435079f8cde3d9ab2
Shovon-07 <aljubair.shovon.07@gmail.com>
2024-03-16 20:03:55 +0600

    c

--- Dashboard-Projects/Admin-Dashboard/src/Pages/Orders/Orders.jsx
+++ Dashboard-Projects/Admin-Dashboard/src/Pages/Orders/Orders.jsx
@@ -5,23 +5,50 @@ import DataTable from "react-data-table-component";
 //___ Css ___//
 import "./Orders.scss";
 
+//___ Images ___//
+import EditeIcon from "/images/icons/view.svg";
+import DeleteIcon from "/images/icons/delete.svg";
+
 //___ Components ___//
-// import TableData from "../../Components/TableData/TableData";
+import Loader from "../../Components/Loader/Loader";
 
 const Orders = () => {
+  const [search, setSearch] = useState("");
   const [orderData, setOrderData] = useState([]);
+  const [filteredOrderData, setfilteredOrderData] = useState([]);
 
   const columns = [
-    { name: "Title", selector: (row) => row.title },
+    { name: "Title", sortable: true, selector: (row) => row.title },
     { name: "Category", selector: (row) => row.category },
     { name: "Price", selector: (row) => row.price },
-    { name: "Rating", selector: (row) => row.rating },
+    { name: "Rating", selector: (row) => row.rating.count },
+    {
+      name: "Action",
+      cell: (row) => (
+        <div className="d-flex" style={{ gap: "10px" }}>
+          <img
+            src={EditeIcon}
+            alt=""
+            style={{ cursor: "pointer" }}
+            onClick={() => alert(row.id)}
+          />
+          <img
+            src={DeleteIcon}
+            alt=""
+            style={{ cursor: "pointer" }}
+            onClick={() => alert(row.id)}
+          />
+        </div>
+      ),
+    },
   ];
 
   const getOrderData = async () => {
     try {
       let res = await axios.get("https://fakestoreapi.com/products");
       setOrderData(res.data);
+      setfilteredOrderData(res.data);
+      console.log(res.data);
     } catch (error) {
       console.log(error);
     }
@@ -31,10 +58,39 @@ const Orders = () => {
     getOrderData();
   }, []);
 
+  useEffect(() => {
+    const result = orderData.filter((filteredOrder) => {
+      return filteredOrder.title.toLowerCase().match(search.toLowerCase());
+    });
+    setfilteredOrderData(result);
+  }, [search]);
+
   return (
     <div className="Orders">
-      Orders
-      <DataTable columns={columns} data={orderData} />
+      {orderData == "" ? <Loader /> : ""}
+      <DataTable
+        columns={columns}
+        data={filteredOrderData}
+        title="Orders"
+        pagination
+        fixedHeader
+        fixedHeaderScrollHeight="400px"
+        selectableRows
+        selectableRowsHighlight
+        highlightOnHover
+        subHeader
+        subHeaderComponent={
+          <input
+            type="text"
+            placeholder="Search here"
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+              console.log(search);
+            }}
+          />
+        }
+      />
     </div>
   );
 };
