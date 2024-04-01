68c432b099cdf96965815c7c101d3b20b4ea0c96
Shovon-07 <aljubair.shovon.07@gmail.com>
2024-04-01 07:11:27 +0600

    C

--- E-Commerce-Projects/E-Commerce-Saudi/src/Components/CustomizeImg/CustomizeImg.jsx
+++ E-Commerce-Projects/E-Commerce-Saudi/src/Components/CustomizeImg/CustomizeImg.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 //___ Images ___//
 import Logo from "/images/icons/Company_orrange.png";
@@ -16,24 +16,71 @@ import Nack_4 from "/images/dress/neck_type/Royal.jpg";
 
 import Pocket_1 from "/images/dress/pocket_type/Curved standard.jpg";
 import Pocket_2 from "/images/dress/pocket_type/Curved.jpg";
-import Pocket_3 from "/images/dress/pocket_type/Hidden curve.jpg";
-import Pocket_4 from "/images/dress/pocket_type/Hidden curve.jpg";
+import Pocket_3 from "/images/dress/pocket_type/Triangle.jpg";
+import Pocket_4 from "/images/dress/pocket_type/Pointed.jpg";
 
-import Hand_1 from "/images/dress/hand_type/Cuffed charts.jpg";
-import Hand_2 from "/images/dress/hand_type/Normal Cuffed.jpg";
-import Hand_3 from "/images/dress/hand_type/Standard Charts.jpg";
-import Hand_4 from "/images/dress/hand_type/Standard small.jpg";
+import Hand_1 from "/images/dress/hand_type/Cuffed.png";
+import Hand_2 from "/images/dress/hand_type/Standard Charts.png";
+import Hand_3 from "/images/dress/hand_type/Standard small.png";
+import Hand_4 from "/images/dress/hand_type/Normal Cuffed.jpg";
 
 //___ Css ___//
 import "./CustomizeImg.scss";
 
 const CustomizeImg = () => {
+  const [btnImgPreview, setBtnImgPreview] = useState();
+  const [pktImgPreview, setPktImgPreview] = useState();
+  const [handImgPreview, setHandImgPreview] = useState();
+
+  const jubba = "/images/dress/jubba2.png";
+  const buttonImg = [
+    { btnImg: Btn_1 },
+    { btnImg: Btn_2 },
+    { btnImg: Btn_3 },
+    { btnImg: Btn_4 },
+    { btnImg: Btn_1 },
+    { btnImg: Btn_2 },
+    { btnImg: Btn_3 },
+    { btnImg: Btn_4 },
+    { btnImg: Btn_1 },
+    { btnImg: Btn_2 },
+    { btnImg: Btn_3 },
+    { btnImg: Btn_4 },
+  ];
+  const pocketImg = [
+    { pktImg: Pocket_1 },
+    { pktImg: Pocket_2 },
+    { pktImg: Pocket_3 },
+    { pktImg: Pocket_4 },
+  ];
+  const handImg = [
+    { hndImg: Hand_1 },
+    { hndImg: Hand_2 },
+    { hndImg: Hand_3 },
+    { hndImg: Hand_4 },
+  ];
+
+  const handelButtonImgPreview = (items) => {
+    setBtnImgPreview(items.btnImg);
+    console.log(items.btnImg);
+  };
+  const handelPocketImgPreview = (items) => {
+    setPktImgPreview(items.pktImg);
+    console.log(items.pktImg);
+  };
+  const handelHandImgPreview = (items) => {
+    setHandImgPreview(items.hndImg);
+    console.log(items.hndImg);
+  };
+
   return (
     <div className="CustomizeImg d-flex flex-start gap-30">
       <div className="left">
         <h2>Preview Image</h2>
-        <div className="imageBox">
-          <img src={Main_img} alt="" className="main-img" />
+        <div className="imageBox d-flex">
+          <img src={jubba} alt="" className="main-img" />
+          <img src={pktImgPreview} alt="" className="pocket" />
+          <img src={handImgPreview} alt="" className="hand" />
         </div>
       </div>
       <div className="right">
@@ -42,30 +89,17 @@ const CustomizeImg = () => {
           <div className="btnParent">
             <button className="button">Button Type</button>
             <ul className="btnChild">
-              <li>
-                <img src={Btn_1} alt="" />
-              </li>
-              <li>
-                <img src={Btn_2} alt="" />
-              </li>
-              <li>
-                <img src={Btn_3} alt="" />
-              </li>
-              <li>
-                <img src={Btn_4} alt="" />
-              </li>
-              <li>
-                <img src={Btn_1} alt="" />
-              </li>
-              <li>
-                <img src={Btn_2} alt="" />
-              </li>
-              <li>
-                <img src={Btn_3} alt="" />
-              </li>
-              <li>
-                <img src={Btn_4} alt="" />
-              </li>
+              {buttonImg.map((items, index) => {
+                return (
+                  <li key={index}>
+                    <img
+                      src={items.btnImg}
+                      alt=""
+                      onClick={() => handelButtonImgPreview(items)}
+                    />
+                  </li>
+                );
+              })}
             </ul>
           </div>
           <div className="btnParent">
@@ -88,35 +122,33 @@ const CustomizeImg = () => {
           <div className="btnParent">
             <button className="button">Pocket Type</button>
             <ul className="btnChild">
-              <li>
-                <img src={Pocket_1} alt="" />
-              </li>
-              <li>
-                <img src={Pocket_2} alt="" />
-              </li>
-              <li>
-                <img src={Pocket_3} alt="" />
-              </li>
-              <li>
-                <img src={Pocket_4} alt="" />
-              </li>
+              {pocketImg.map((items, index) => {
+                return (
+                  <li key={index}>
+                    <img
+                      src={items.pktImg}
+                      alt=""
+                      onClick={() => handelPocketImgPreview(items)}
+                    />
+                  </li>
+                );
+              })}
             </ul>
           </div>
           <div className="btnParent">
             <button className="button">Hand Type</button>
             <ul className="btnChild">
-              <li>
-                <img src={Hand_1} alt="" />
-              </li>
-              <li>
-                <img src={Hand_2} alt="" />
-              </li>
-              <li>
-                <img src={Hand_3} alt="" />
-              </li>
-              <li>
-                <img src={Hand_4} alt="" />
-              </li>
+              {handImg.map((items, index) => {
+                return (
+                  <li key={index}>
+                    <img
+                      src={items.hndImg}
+                      alt=""
+                      onClick={() => handelHandImgPreview(items)}
+                    />
+                  </li>
+                );
+              })}
             </ul>
           </div>
         </div>
