a96aca606c9afaf7a48eec08e91e2c63e7d85f7f
Shovon-07 <aljubair.shovon.07@gmail.com>
2024-08-21 15:21:16 +0600

    C

--- E-Commerce-Projects/E-Commerce/src/Pages/LearnReact/LearnUseReducer.jsx
+++ E-Commerce-Projects/E-Commerce/src/Pages/LearnReact/LearnUseReducer.jsx
@@ -1,26 +1,36 @@
 import React, { useReducer } from "react";
 
-const initialState = 0;
-const reducer = (state, action) => {
-  switch (action) {
-    case "inc":
-      return state + 1;
-    case "dec":
-      return state - 1;
-    default:
-      return state;
-  }
-};
-
 const LearnUseReducer = () => {
-  const [reducerCounter, dispatch] = useReducer(reducer, initialState);
+  const initialState = 0;
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "INCREMENT":
+        return state + 1;
+      case "DECREMENT":
+        return state - 1;
+      default:
+        return state;
+    }
+  };
+  const [counter, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div>
       <h1>Learn Use Reducer</h1>
-      <p>Reducer counter = {reducerCounter}</p>
-      <button onClick={() => dispatch("inc")}>Increment</button> ||{" "}
-      <button onClick={() => dispatch("dec")}>Decrement</button>
+      <p>Counter value = {counter}</p>
+      <button
+        className="button"
+        onClick={() => dispatch({ type: "INCREMENT" })}
+      >
+        INCREMENT
+      </button>{" "}
+      | {""}
+      <button
+        className="button"
+        onClick={() => dispatch({ type: "DECREMENT" })}
+      >
+        DECREMENT
+      </button>
     </div>
   );
 };
